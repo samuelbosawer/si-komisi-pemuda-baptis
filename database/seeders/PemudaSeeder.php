9f25ea7edfade2459c5b14ec6366ef51424ee99c
Samuel Bosawer <zam.bosawer@gmail.com>
2024-08-28 12:11:29 +0900

    feat : buat controller, model, migratsion dan seedr

---
+++ database/seeders/PemudaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PemudaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
