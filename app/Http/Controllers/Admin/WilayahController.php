237ec14a103492e92762c0f23606ad5084b412c4
Samuel Bosawer <zam.bosawer@gmail.com>
2024-09-01 11:46:38 +0900

    FeatL CRUD Wilayah

--- app/Http/Controllers/Admin/WilayahController.php
+++ app/Http/Controllers/Admin/WilayahController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Wilayah;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\File;
 
 class WilayahController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $datas = Wilayah::where([
+            ['nama_wilayah', '!=', Null],
+            [function ($query) use ($request) {
+                if (($s = $request->s)) {
+                    $query->orWhere('nama_wilayah', 'LIKE', '%' . $s . '%')
+                        ->orWhere('kode_wilayah', 'LIKE', '%' . $s . '%')
+                        ->orWhere('keterangan', 'LIKE', '%' . $s . '%');
+                }
+            }]
+        ])->orderBy('id', 'desc')->paginate(10);
+        return view('admin.wilayah.index',compact('datas'))->with('i',(request()->input('page', 1) - 1) * 10);
     }
 
     /**
@@ -20,7 +33,7 @@ class WilayahController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.wilayah.create');
     }
 
     /**
@@ -28,7 +41,23 @@ class WilayahController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $this->validate($request, [
+            'nama_wilayah' => 'required',
+        ],
+        [
+            'nama_wilayah.required' => 'Tidak boleh kosong',
+        ]
+        );
+        $data = new Wilayah();
+
+        $data->nama_wilayah   = $request->nama_wilayah;
+        $data->kode_wilayah   = $request->kode_wilayah;
+        $data->keterangan   = $request->keterangan;
+
+        $data->save();
+        alert()->success('Berhasil', 'Tambah data berhasil')->autoclose(3000);
+        return redirect()->route('admin.wilayah');
     }
 
     /**
@@ -36,7 +65,9 @@ class WilayahController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data = Wilayah::where('id',$id)->first();
+        $caption = 'Detail Data Wilayah';
+        return view('admin.wilayah.create',compact('data','caption'));
     }
 
     /**
@@ -44,7 +75,9 @@ class WilayahController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $data = Wilayah::where('id',$id)->first();
+        $caption = 'Ubah Data Wilayah';
+        return view('admin.wilayah.create',compact('data','caption'));
     }
 
     /**
@@ -52,7 +85,22 @@ class WilayahController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $this->validate($request, [
+            'nama_wilayah' => 'required',
+        ],
+        [
+            'nama_wilayah.required' => 'Tidak boleh kosong',
+        ]
+        );
+        $data = Wilayah::find($id);
+
+        $data->nama_wilayah   = $request->nama_wilayah;
+        $data->kode_wilayah   = $request->kode_wilayah;
+        $data->keterangan   = $request->keterangan;
+
+        $data->update();
+        alert()->success('Berhasil', 'Ubah data berhasil')->autoclose(3000);
+        return redirect()->route('admin.wilayah');
     }
 
     /**
@@ -60,6 +108,8 @@ class WilayahController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $data = Wilayah::find($id);
+        $data->delete();
+        return redirect()->back();
     }
 }
---
+++ resources/views/admin/wilayah/create.blade.php
@@ -0,0 +1,152 @@
+@extends('admin.layout.tamplate')
+@section('title')
+    Wilayah
+@endsection
+@section('content')
+    <!-- ============================================================== -->
+    <!-- Start Page Content here -->
+    <!-- ============================================================== -->
+
+    <div class="content-page">
+        <div class="content">
+
+            <!-- Start Content-->
+            <div class="container-fluid">
+
+                @include('admin.layout.breadcump')
+
+                <div class="row">
+                    <div class="col-12 ">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="header-title"> {{ $caption ?? 'Tambah Data Wilayah' }} </h4>
+
+
+                                @if (Request::segment(4) == 'ubah')
+                                    <form action="{{ route('admin.wilayah.update', $data->id) }}" method="post"
+                                        enctype="multipart/form-data">
+                                        @method('PUT')
+                                    @else
+                                        <form action="{{ route('admin.wilayah.store') }}" method="post"
+                                            enctype="multipart/form-data">
+                                @endif
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="card-box">
+                                            <div class="row">
+
+                                                <div class="col-md-4">
+                                                    <div class="form-group mb-3">
+                                                        <label for="nama_wilayah"> Nama Wilayah <span
+                                                                class="text-danger">*</span> </label>
+                                                        <input type="text" id="nama_wilayah"
+                                                            @if (Request::segment(3) == 'detail') {{ 'disabled' }} @endif
+                                                            value="{{ old('nama_wilayah') ?? ($data->nama_wilayah ?? '') }}"
+                                                            name="nama_wilayah" placeholder="" class="form-control">
+                                                        @if ($errors->has('nama_wilayah'))
+                                                            <label class="text-danger">
+                                                                {{ $errors->first('nama_wilayah') }}
+                                                            </label>
+                                                        @endif
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-4">
+                                                    <div class="form-group mb-3">
+                                                        <label for="kode_wilayah"> Kode Wilayah <span class="text-danger">
+                                                            </span> </label>
+                                                        <input type="text" id="kode_wilayah"
+                                                            @if (Request::segment(3) == 'detail') {{ 'disabled' }} @endif
+                                                            value="{{ old('kode_wilayah') ?? ($data->kode_wilayah ?? '') }}"
+                                                            name="kode_wilayah" placeholder="" class="form-control">
+                                                        @if ($errors->has('kode_wilayah'))
+                                                            <label class="text-danger">
+                                                                {{ $errors->first('kode_wilayah') }}
+                                                            </label>
+                                                        @endif
+                                                    </div>
+                                                </div>
+
+
+                                                <div class="col-md-8">
+                                                    <div class="form-group mb-3">
+                                                        <label for="keterangan"> Keterangan </label>
+                                                        <textarea id="keterangan" @if (Request::segment(3) == 'detail') disabled @endif name="keterangan"
+                                                            placeholder="Masukan keterangan" rows="5" class="form-control">{{ old('keterangan') ?? ($data->keterangan ?? '') }} </textarea>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+
+
+
+
+
+
+                                            @if (Request::segment(3) == 'detail')
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <a class="btn btn-primary"
+                                                            href="{{ route('admin.pemuda') }}">Kembali</a>
+                                                        <a class="btn btn-primary"
+                                                            href="{{ route('admin.wilayah.ubah', $data->id) }}">Ubah <i
+                                                                class="fas fa-edit"></i> </a>
+                                                    </div>
+                                                </div>
+                                            @else
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <button type="submit" class="btn btn-primary">Simpan <i
+                                                                data-feather="save"></i></button>
+                                                    </div>
+                                                </div>
+                                            @endif
+
+                                        </div>
+
+                                    </div>
+
+
+
+
+                                </div> <!-- end card-box-->
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+                        </form>
+
+
+
+
+                    </div> <!-- end card-body-->
+                </div> <!-- end card-->
+            </div> <!-- end col -->
+        </div>
+        {{-- end row --}}
+
+
+
+
+
+    </div> <!-- container -->
+
+    </div> <!-- content -->
+@endsection
+
+@push('script-footer')
+    <script src="/assets/js/pages/form-fileuploads.init.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(e) {
+            $('#images').change(function() {
+                let reader = new FileReader();
+                reader.onload = (e) => {
+                    $('#preview-images').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(this.files[0]);
+            });
+
+        });
+    </script>
+@endpush
